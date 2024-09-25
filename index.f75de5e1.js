11a1cf331a86881044d14e20c0d9d09df8b8296c
Korshun <dmitriy.kor91@gmail.com>
2024-09-25 10:12:09 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+const allGallery = document.querySelector("ul");
+allGallery.addEventListener("click", (even)=>{
+    const link = even.target.closest("a");
+    if (link) {
+        even.preventDefault();
+        const mainPhoto = document.getElementById("largeImg");
+        mainPhoto.src = link.href;
+    }
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
